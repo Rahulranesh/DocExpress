fdaf06be129b9b9e41751cd1d408ad15533b33ec
Rahulranesh <rahulranesh590@gmail.com>
2026-04-12 00:01:36 +0530

    feat: Update project dependencies and improve app description

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 val keystoreProperties = Properties()
@@ -22,7 +21,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -63,8 +61,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
